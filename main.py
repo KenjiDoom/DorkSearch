1550a6cefd6a071e3d0bdc3165beb506ac5ba78e
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-07-26 15:14:06 -0700

    saving to a file

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ import re, json, httpx, argparse
 
 # User option here, but using command line
 parser = argparse.ArgumentParser(description='Use a custom dork list and search a website')
-parser.add_argument('--url', type=str, action='store')
+parser.add_argument('--url', type=str, action='store', required=True)
+parser.add_argument('--output', type=str, action='store')
 args = parser.parse_args()
 
-def main(url):
+def main(url, output=None):
+    valid_urls = []
     print('Performing scan...')
     with DDGS(timeout=3) as ddgs:
-        for r in ddgs.text(url):
-            print(r['href'])
-main(args.url)
+        for data in ddgs.text(url):
+            valid_urls.append(data['href'])
+    if output == None:
+        pass
+    elif output != None:
+        print('Saving results to file...')
+        with open(output + '.txt', 'w') as r:
+            r.write(str(valid_urls))
+            r.close()
+
+main(args.url, args.output)
+
 
