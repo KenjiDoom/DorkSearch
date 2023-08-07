7d82f8b84f734ed897d096cf594a36c551164e0b
kenjidoom <77996865+KenjiDoom@users.noreply.github.com>
2023-08-07 15:43:16 -0700

    changed the logic of the program

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ parser.add_argument('--url', type=str, action='store', required=True)
 parser.add_argument('--dork', type=str, action='store', required=True)
 parser.add_argument('--output', type=str, action='store')
 parser.add_argument('--proxy', type=str, action='store')
-#args = parser.parse_args()
+args = parser.parse_args()
 
-def main(site, dork_file, proxy_list, output=None):
+def main(site, dork_file, proxy_file, output=None):
     headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
@@ -19,28 +19,31 @@ def main(site, dork_file, proxy_list, output=None):
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     }
     valid_urls = []
+    # 1. Check the proxy file
+    # 2. Check the proxies from the file and make sure they are valid.
+    # 3. Get a random proxy
+    proxy_list = check_proxy_type(proxy_file)    
     with open(str(dork_file), 'r') as f:
        dorks = ['inurl:' + line.strip() for line in f]
        for dork in dorks:
-        print('Waiting 1 second...')
+        print("Starting....")
         sleep(1)
         proxy = grab_random_proxy(proxy_list)
-        print(type(proxy))
         if proxy != None:
-            print(f'Using the following proxy: {proxy}')
+            print(f'Fetching data with the following proxy: {proxy}')
             page = requests.get(f'https://duckduckgo.com/html/?q=site:{site} inurl:{dork}', proxies={'https': f"socks5://{proxy}"}, headers=headers).text
             sleep(5)
-            print('Waiting 5 seconds...')
             soup = BeautifulSoup(page, 'html.parser')
             data = soup.find_all("a", class_="result__url", href=True)
         elif proxy == None:
-            print('Not using a proxy')
+            print('Fetching data without proxy...')
             page = requests.get(f'https://duckduckgo.com/html/?q=site:{site} inurl:{dork}', headers=headers).text
             sleep(5)
-            print('Waiting 5 seconds...')
             soup = BeautifulSoup(page, 'html.parser')
             data = soup.find_all("a", class_="result__url", href=True)
-                   
+        elif proxy == 'Tor':
+            print('Running tor...')
+
         if "If this persists, please" in page:
             print('Dork is not found' + str(dork))
         else:
@@ -66,8 +69,7 @@ def grab_random_proxy(proxy_list):
         random_proxy = proxy_list[random_index_number_proxy_list]
         return random_proxy
     except ValueError:
-        # No because we are still returning a list...., we want to return a string
-        return ""
+        return None
 
 def check_proxy(proxy):
     try:
@@ -93,11 +95,10 @@ def check_proxy_type(proxy):
             valid_proxies.append(check_proxy(proxy))
     except FileNotFoundError:
         print('File not found....')
-    main('https://bloody.com/', 'dork.txt', valid_proxies)
+    return valid_proxies
 
 
-check_proxy_type('test.txt')
-#main('https://bloody.com/', 'dork.txt', valid_proxies)
-
+if __name__ == '__main__':
+    main(args.url, args.dork, args.proxy, args.output)
 
 
